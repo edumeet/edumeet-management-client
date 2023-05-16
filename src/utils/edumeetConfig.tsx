ea365c4734960877f4fb86c83de61648ca814df3
Rémai Gábor <remai.gabor@gmail.com>
2023-05-16 12:24:49 +0200

    demo for testing

---
+++ src/utils/edumeetConfig.tsx
@@ -0,0 +1,41 @@
+import { defaultEdumeetConfig, EdumeetConfig } from './types';
+
+declare module '@mui/material/styles' {
+	interface Theme {
+		backgroundImage?: string;
+		background?: string;
+		appBarColor: string;
+		logo: string;
+		activeSpeakerBorder: string;
+		videoBackroundColor: string;
+		videoShadow: string;
+		videoAvatarImage: string;
+		videoRoundedCorners: boolean;
+		chatColor?: string;
+	}
+
+	interface ThemeOptions {
+		backgroundImage?: string;
+		background?: string;
+		appBarColor?: string;
+		logo?: string;
+		activeSpeakerBorder?: string;
+		videoBackroundColor?: string;
+		videoShadow?: string;
+		videoAvatarImage?: string;
+		videoRoundedCorners?: boolean;
+		chatColor?: string;
+	}
+}
+
+declare global {
+	interface Window {
+		config?: Partial<EdumeetConfig>;
+	}
+}
+
+export default {
+	...defaultEdumeetConfig,
+	...window.config,
+	theme: { ...defaultEdumeetConfig.theme, ...window.config?.theme }
+};
