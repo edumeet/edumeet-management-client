dd3160fd82b3f811bb2429662db2a48ace48f4bb
Rémai Gábor <remai.gabor@gmail.com>
2023-09-18 16:55:30 +0200

    update login page

--- src/components/login/loginPage.tsx
+++ src/components/login/loginPage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 import Box from '@mui/material/Box';
 
 import io from 'socket.io-client';
@@ -7,8 +7,12 @@ import socketio from '@feathersjs/socketio-client';
 import authentication from '@feathersjs/authentication-client';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
-import { FormControl, FormLabel } from '@mui/material';
 import edumeetConfig from '../../utils/edumeetConfig';
+
+import CssBaseline from '@mui/material/CssBaseline';
+
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 const socket = io(edumeetConfig.hostname, { path: edumeetConfig.path });
 // Initialize our Feathers client application through Socket.io
@@ -19,50 +23,82 @@ client.configure(socketio(socket));
 // Use localStorage to store our login token
 client.configure(authentication());
 
-const drawerWidth = 20;
+// TODO remove, this demo shouldn't need to reset the theme.
+const defaultTheme = createTheme();
 
-function LoginPage() {
+export default function SignIn() {
+	const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		const data = new FormData(event.currentTarget);
+
+		const email = data.get('email'); // typechecks!
+		const password = data.get('password'); // typechecks!
+
+		try {
+			// Authenticate with the local email/password strategy
+			await client.authenticate({
+				strategy: 'local',
+				email: email,
+				password: password
+			});
+			// Show e.g. logged in dashboard page
+			window.location.reload();
+		} catch (error) {
+			// Show login page (potentially with `e.message`)
+		}
+		
+	};
+
 	return (
-		<Box
-			component="main"
-			sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - ${drawerWidth}px)` } }}
-		>
-				https://edumeet.example.com/mgmt/oauth/tenant?tenantId=1
-			<form
-				// ref={formRef}
-				onSubmit={async (e: React.SyntheticEvent) => {
-					e.preventDefault();
-					const target = e.target as typeof e.target & {
-                        email: { value: string };
-                        password: { value: string };
-                    };
-					const email = target.email.value; // typechecks!
-					const password = target.password.value; // typechecks!
+		<ThemeProvider theme={defaultTheme}>
+			<Container component="main" maxWidth="xs">
+				<CssBaseline />
+				<Box
+					sx={{
+						marginTop: 8,
+						display: 'flex',
+						flexDirection: 'column',
+						alignItems: 'center',
+					}}
+				>
+					<img src='https://raw.githubusercontent.com/edumeet/edumeet/master/app/public/images/logo.edumeet.svg' />
 
-					try {
-						// Authenticate with the local email/password strategy
-						await client.authenticate({
-							strategy: 'local',
-							email: email,
-							password: password
-						});
-						// Show e.g. logged in dashboard page
-						window.location.reload();
-					} catch (error) {
-						// Show login page (potentially with `e.message`)
-					}
-				}}
-			>
-				<FormControl>
-					<h3>Login page</h3>
-					<FormLabel>Enter Email</FormLabel>
-					<TextField name='email' type="email"></TextField>
-					<FormLabel>Password</FormLabel>
-					<TextField name='password' type="password"></TextField>
-					<Button type='submit' >Submit</Button>
-				</FormControl>
-			</form>
-		</Box>
+					<Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+						<TextField
+							margin="normal"
+							required
+							fullWidth
+							id="email"
+							label="Email Address"
+							name="email"
+							autoComplete="email"
+							autoFocus
+						/>
+						<TextField
+							margin="normal"
+							required
+							fullWidth
+							name="password"
+							label="Password"
+							type="password"
+							id="password"
+							autoComplete="current-password"
+						/>
+						{/* <FormControlLabel
+							control={<Checkbox value="remember" color="primary" />}
+							label="Remember me"
+						/> */}
+						<Button
+							type="submit"
+							fullWidth
+							variant="contained"
+							sx={{ mt: 3, mb: 2 }}
+						>
+						Sign In
+						</Button>
+					</Box>
+				</Box>
+			</Container>
+		</ThemeProvider>
 	);
 }
-export default LoginPage;
